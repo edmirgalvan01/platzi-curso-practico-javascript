78379c9f38057de4f4f0148673ce2404c389a9c4
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-07-23 10:47:34 -0500

    Hice la interaccion con el usuario y ya funcionan todas las formulas

--- figuras-geometricas/figuras.js
+++ figuras-geometricas/figuras.js
@@ -14,3 +14,55 @@ const perimetroCirculo = (radio) => {
    return diametro * pi;
 };
 const areaCirculo = (radio) => pi * (radio * radio);
+
+// Empezamos a interactuar con HTML
+//Area y perimetro del cuadrado
+const calcularPerimetroCuadrado = () => {
+   const input = document.getElementById('inputCuadrado'); //Tomamos la etiqueta del input por su ID
+   const value = input.value; //Tomamos el valor que el usuario escribio en el input
+   const perimetro = perimetroCuadrado(value); //Calculamos perimetro mediante el valor del usuario
+   alert(perimetro);
+};
+
+const calcularAreaCuadrado = () => {
+   const input = document.getElementById('inputCuadrado'); //Tomamos la etiqueta del input por su ID
+   const value = input.value; //Tomamos el valor que el usuario escribio en el input
+   const area = areaCuadrado(value); //Calculamos perimetro mediante el valor del usuario
+   alert(area);
+};
+//Area y perimetro del cuadrado
+
+//Area y perimetro del triangulo
+const calcularPerimetroTriangulo = () => {
+   const lado1 = parseInt(document.getElementById('inputLado1Triangulo').value); //Tomamos el valor del lado 1
+   const lado2 = parseInt(document.getElementById('inputLado2Triangulo').value); //Tomamos el valor del lado 2
+   const base = parseInt(document.getElementById('inputBaseTriangulo').value); //Tomamos el valor de la base
+
+   const perimetro = perimetroTriangulo(lado1, lado2, base);
+};
+
+const calcularAreaTriangulo = () => {
+   const base = parseInt(document.getElementById('inputBaseTriangulo').value); //Tomamos el valor de la base
+   const altura = parseInt(
+      document.getElementById('inputAlturaTriangulo').value
+   ); //Tomamos el valor de la altura
+   const area = areaTriangulo(base, altura);
+};
+//Area y perimetro del triangulo
+
+//Diametro. perimetro y area del circulo
+const calcularDiametroCirculo = () => {
+   const radio = parseInt(document.getElementById('inputRadioCirculo').value);
+   const diametro = diametroCirculo(radio);
+};
+
+const calcularPerimetroCirculo = () => {
+   const radio = parseInt(document.getElementById('inputRadioCirculo').value);
+   const perimetro = perimetroCirculo(radio);
+};
+
+const calcularAreaCirculo = () => {
+   const radio = parseInt(document.getElementById('inputRadioCirculo').value);
+   const area = areaCirculo(radio);
+};
+//Diametro. perimetro y area del circulo

c584f5bc6deb5011b5d34b3fdf5f0574c0fe6ef1
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-07-26 10:01:49 -0500

    Hice la funcion que calcula los descuentos

--- precios-descuentos/descuentos.js
+++ precios-descuentos/descuentos.js
@@ -1,1 +1,5 @@
-
+function calcularPrecioConDescuento(precio, descuento) {
+   const porcentajePrecioConDescuento = 100 - descuento;
+   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+   return precioConDescuento;
+}

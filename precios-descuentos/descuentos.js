0bff1c9171c073e6490a0f26f3e50e70d8ce8712
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-07-26 11:22:14 -0500

    Introduje la funcion para que se añadiera descuento por cupon al programa

--- precios-descuentos/descuentos.js
+++ precios-descuentos/descuentos.js
@@ -1,5 +1,34 @@
-function calcularPrecioConDescuento(precio, descuento) {
-   const porcentajePrecioConDescuento = 100 - descuento;
+const listaCupones = {
+   Edmir01: 10,
+   Edmir02: 15,
+   Epin01: 20,
+}; //Lista de los cupones
+
+function descuentoDelCupon() {
+   const nombreCupon = document.getElementById('inputCupon').value;
+   const cuponDefault = 0;
+   const cuponDescuento = listaCupones[nombreCupon] || cuponDefault;
+   return cuponDescuento;
+} //Esta funcion obtiene cual es el total del cupon
+
+function calcularPrecioConDescuento(precio, descuento, cupon) {
+   const descuentoConCupon = descuento + cupon;
+   const porcentajePrecioConDescuento = 100 - descuentoConCupon;
    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
    return precioConDescuento;
+} //Esta funcion tiene la formula para hacer el descuento
+
+function onClickButtonPrecioDescuento() {
+   const precio = parseInt(document.getElementById('inputPrecio').value); //Nos traemos el valor del input y cambiamos su tipo a numero
+   const descuento = parseInt(document.getElementById('inputDescuento').value); //Nos traemos el valor del input y cambiamos su tipo a numero
+   const cupon = descuentoDelCupon(); //Nos traemos el valor del cupon que arroja la funcion
+
+   const precioConDescuento = calcularPrecioConDescuento(
+      precio,
+      descuento,
+      cupon
+   );
+
+   document.getElementById('resultadoDelDescuento').innerText = //Añadimos el resultado al parrafo mediante su ID
+      'El precio con descuento es de $' + precioConDescuento;
 }
